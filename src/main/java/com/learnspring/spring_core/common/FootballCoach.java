293b59e25182ded4b415e0977aef06909c360fcc
Omar Hosny <102857390+Omar-Hosny1@users.noreply.github.com>
2024-01-16 19:58:10 +0200

    added the lazy initialization

--- src/main/java/com/learnspring/spring_core/common/FootballCoach.java
+++ src/main/java/com/learnspring/spring_core/common/FootballCoach.java
@@ -1,5 +1,6 @@
 package com.learnspring.spring_core.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
--- src/main/java/com/learnspring/spring_core/rest/DemoController.java
+++ src/main/java/com/learnspring/spring_core/rest/DemoController.java
@@ -3,16 +3,21 @@ package com.learnspring.spring_core.rest;
 import com.learnspring.spring_core.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Lazy
 public class DemoController {
     private Coach myCoach;
 
     @Autowired
     public DemoController(@Qualifier("tennisCoach") Coach theCoach) {
         myCoach = theCoach;
+        System.out.println("This Class " + this.getClass().getName());
     }
 
     @GetMapping("/dailyworkout")
